9b7d4771347d23d2d0aaabe3e2ff5d3e1be00b89
songhee <etesong0114@gmail.com>
2023-07-26 00:35:03 +0900

    주석 설명 추가

--- 8.flask_orm/app.py
+++ 8.flask_orm/app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request, redirect, session, url_for, flash
-from datetime import timedelta
+from flask import Flask, render_template, request
+from flask import redirect, session, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+
+from datetime import timedelta
+
 
 app = Flask(__name__)
 app.permanent_session_lifetime = timedelta(minutes=1)
@@ -11,18 +14,25 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-class Users(db.Model):
-    def __init__(self, name, password, email):
-        self.name = name
-        self.password = password
-        self.email = email
+# 테이블 이름은 소문자로 변환하고 이를 테이블 이름으로 사용하여 클래스 이름에서 파생되거나
+# 내부에 __tablename__ = 'users' 로 지정할 수 있다.
 
+# Python 클래스 내부에서 _id라는 이름으로 열(Column)을 정의
+# but "id"를 통해 데이터베이스에서 이 열의 이름은 "id"로 지정
+# 즉, 데이터베이스에서는 "id"라는 이름으로 해당 열을 저장
+class Users(db.Model):
     _id = db.Column("id", db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     password = db.Column(db.String(100))
     email = db.Column(db.String(100))
 
-    
+    def __init__(self, name, password, email):
+        self.name = name
+        self.password = password
+        self.email = email
+users = Users.query.all()
+
+   
 users = {
     'user1': {'password': 'password123'},
     'user2': {'password': 'abc123'}
@@ -47,11 +57,13 @@ def login():
         username = request.form['username']
         password = request.form['password']
 
+        # 데이터 조회
         found_user = Users.query.filter_by(name=username).first()
         if found_user:
             flash("Login Successful")
 
         else:
+            # 데이터베이스에 데이터 추가
             user = Users(username, password, "")
             db.session.add(user)
             db.session.commit()
@@ -69,6 +81,7 @@ def delete():
     if request.method == 'POST':
         action = request.form["action"]
         if action == "DELETE":
+            # 데이터베이스에 데이터 삭제
             Users.query.filter_by(name=user).delete()
             db.session.commit()
             return redirect(url_for('logout'))
