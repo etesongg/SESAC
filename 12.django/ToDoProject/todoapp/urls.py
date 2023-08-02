2b3be60d052026cad82ac7b8f7717e7bbbf57003
songhee <etesong0114@gmail.com>
2023-08-02 12:23:34 +0900

    강사님 수업듣고 수정, 추가한 코드

--- 12.django/ToDoProject/todoapp/urls.py
+++ 12.django/ToDoProject/todoapp/urls.py
@@ -5,7 +5,17 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('todo/', views.todo, name='todo'),
     path('create/', views.create, name='create'),
-    path('todo/<int:id>', views.todo_view, name='todo_view'),
-    path('update/<int:id>', views.update, name='update'),
-    path('delete/<int:id>', views.delete, name='delete')
+    path('<int:id>', views.todo_view, name='todo_view'), # todo/<int:id> todo 생략 가능
+    path('<int:id>/update', views.update, name='update'),
+    path('<int:id>/delete', views.delete, name='delete')
 ]
+
+# 현재 
+# todo/create
+# todo/<tid>/
+# todo/<tid>/update
+# todo/<tid>/delete
+
+# todo/<tid>/  method:CRUD 이런식의 url이어야 함
+# method == delete 이런식으로 받아서 처리
+# 결과적으로 path로 todo/, todo/<int:id>만 남아 있어야 함
--- 12.django/ToDoProject/todoapp/views.py
+++ 12.django/ToDoProject/todoapp/views.py
@@ -1,15 +1,14 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import ToDo
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'index.html')
+    return redirect('todo')
 
 def todo(request):
-    # db로 부터 메세지 받아오기 (orm을 사용해서)
     todos = ToDo.objects.all()
-    return render(request, 'todo.html', {'todos' : todos})
+    return render(request, 'todo/todo.html', {'todos' : todos})
 
 def create(request):
     if request.method == 'POST':
@@ -18,24 +17,33 @@ def create(request):
         ToDo.objects.create(title=title, description=description)
         return redirect('todo')
 
-    return render(request, 'create.html')
+    return render(request, 'todo/create.html')
 
 def todo_view(request, id):
-    todo = ToDo.objects.get(id=id)
-    return render(request, 'todo_view.html', {'todo': todo})
+    # todo = ToDo.objects.get(id=id)
+    
+    # 가져오는 여러방식 있음
+    # 없는 페이지를 요청할때 만약 목록 5가 없는데 url에서 직접 /5로 치고 들어간다면 404로 보내줌
+    todo = get_object_or_404(ToDo, id=id)
+
+    return render(request, 'todo/todo_view.html', {'todo': todo})
 
 def update(request, id):
-    todo = ToDo.objects.get(id=id)
+    # todo = ToDo.objects.get(id=id)
+    todo = get_object_or_404(ToDo, id=id)
+
     if request.method == 'POST':
         new_title = request.POST['new_title']
         new_description = request.POST['new_description']
         todo.title = new_title
         todo.description = new_description
         todo.save()
-        return redirect('todo')
+        return redirect('todo_view', todo.id) # todo.id 대신 id=id 써도 가능
 
-    return render(request, 'update.html', {'todo': todo})
+    return render(request, 'todo/update.html', {'todo': todo})
 
 def delete(request, id):
-    ToDo.objects.filter(id=id).delete()
+    # ToDo.objects.filter(id=id).delete()
+    todo = get_object_or_404(ToDo, id=id)
+    todo.delete()
     return redirect('todo')
