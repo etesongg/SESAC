0192751dbd57ffdb9a970ce3ebbc2e5797b3cdf5
etesong014 <etesong0114@gmail.com>
2023-08-09 20:38:52 +0900

    8.9수업& 무비랭킹 js 수정

--- 14.Web/daily_movie/static/main.js
+++ 14.Web/daily_movie/static/main.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var span_texts = document.querySelectorAll("#text");
+    const span_texts = document.querySelectorAll("#text");
     span_texts.forEach(function(span_text) {
-      var maxlen = 100; 
-      var text = span_text.textContent;
+      const maxlen = 100; 
+      const text = span_text.textContent;
       
       if (text.length > maxlen) {
-        var truncatedText = text.substring(0, maxlen) + "...";
+        const truncatedText = text.substring(0, maxlen) + "...";
         span_text.textContent = truncatedText;
       }
     });
